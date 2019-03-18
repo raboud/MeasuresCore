77010fe5740b3030b6fa0bcabed79c04a6aee460
Robert <robert.raboud@randreng.com>
2019-03-18 10:06:38 -0400

    Settup for GrayLog Serilog logging

--- MeasureCore.MVC/Program.cs
+++ MeasureCore.MVC/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace MeasureCore.MVC
 {
@@ -24,6 +26,7 @@ namespace MeasureCore.MVC
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.CaptureStartupErrors(false);
+                    webBuilder.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration) );
                     webBuilder.UseStartup<Startup>();
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
                     webBuilder.UseConfiguration(configuration);
